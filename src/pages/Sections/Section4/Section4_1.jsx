e6049b8cc93029bb2674e6e885c7a943d30eba86
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-12-01 20:58:11 -0500

    Modified section 4.1; Now results are stored in local storage

--- src/pages/Sections/Section4/Section4_1.jsx
+++ src/pages/Sections/Section4/Section4_1.jsx
@@ -1,52 +1,62 @@
 // ./src/pages/Sections/Section4/Formulario4_1.js
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { questions4_1 as form4_1Questions } from "../../../questions/questions4/questions4_1"; // Importación del arreglo de componentes
-import {operationQuestions} from "../../../questions/operation_questions"; // Importación del arreglo de componentes
+import { operationQuestions } from "../../../questions/operation_questions"; // Importación del arreglo de componentes
 import FormWrapper from "../../../components/FormWrapper"; // Asegúrate de tener este componente
 
 const Formulario4_1 = () => {
-  const [formData, setFormData] = useState({
-    // Preguntas de Operation
-    nombreOperacion: "",
-    dependencia: "",
-    registroAdmin: "",
-    objetivo: "",
-    poblacion: "",
-    anioInicio: "",
-    areaTematica: "",
-    periodicidad: "",
-    unidadEstudio: "",
-    variablesPrincipales: "",
-    coberturaGeografica: "",
-    periodoReferencia: "",
-    metodoRecoleccion: "",
-    
-    // Preguntas 4.1
-    respRecursosPregunta1: "",
-    respRecursosPregunta2: [],
-    respRecursosEvidencia1: "",
-    respRecursosEvidencia2: "",
-    respRecursosEvidencia3: "",
-    respMejoraPregunta1: "",
-    evaluacionPregunta1: 3,
-    respMejoraEvidencia1: "",
-    respMejoraEvidencia2: "",
-    respMejoraEvidencia3: "",
-    asignacionPregunta1: "",
-    asignacionPregunta2: [],
-    asignacionEvidencia1: "",
-    asignacionEvidencia2: "",
-    asignacionEvidencia3: "",
-    evaluacionPregunta2: 3,
-    evaluacionEvidencia1: "",
-    evaluacionEvidencia2: "",
-    evaluacionEvidencia3: "",
-    sugerenciasComentario: "",
-    accion: "",
-    responsableSNS: "",
-    fechaCumplimiento: "",
+  const localStorageKey = "formulario4_1";
+
+  const [formData, setFormData] = useState(() => {
+    // Load data from LocalStorage
+    const savedData = localStorage.getItem(localStorageKey);
+    return savedData
+      ? JSON.parse(savedData)
+      : {
+          nombreOperacion: "",
+          dependencia: "",
+          registroAdmin: "",
+          objetivo: "",
+          poblacion: "",
+          anioInicio: "",
+          areaTematica: "",
+          periodicidad: "",
+          unidadEstudio: "",
+          variablesPrincipales: "",
+          coberturaGeografica: "",
+          periodoReferencia: "",
+          metodoRecoleccion: "",
+          respRecursosPregunta1: "",
+          respRecursosPregunta2: [],
+          respRecursosEvidencia1: "",
+          respRecursosEvidencia2: "",
+          respRecursosEvidencia3: "",
+          respMejoraPregunta1: "",
+          evaluacionPregunta1: 3,
+          respMejoraEvidencia1: "",
+          respMejoraEvidencia2: "",
+          respMejoraEvidencia3: "",
+          asignacionPregunta1: "",
+          asignacionPregunta2: [],
+          asignacionEvidencia1: "",
+          asignacionEvidencia2: "",
+          asignacionEvidencia3: "",
+          evaluacionPregunta2: 3,
+          evaluacionEvidencia1: "",
+          evaluacionEvidencia2: "",
+          evaluacionEvidencia3: "",
+          sugerenciasComentario: "",
+          accion: "",
+          responsableSNS: "",
+          fechaCumplimiento: "",
+        };
   });
+
+  useEffect(() => {
+    // Save data to LocalStorage on every change
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }, [formData]);
 
   // Función para manejar cambios en inputs de tipo texto, radio y checkbox
   const handleChange = (e) => {
@@ -96,8 +106,16 @@ const Formulario4_1 = () => {
     <FormWrapper
       mainTitle={mainTitle}
       introductions={introductions}
-      operationQuestions={operationQuestions(formData, handleChange, handleSelectChange)}
-      formQuestions={form4_1Questions(formData, handleChange, handleSelectChange)}
+      operationQuestions={operationQuestions(
+        formData,
+        handleChange,
+        handleSelectChange
+      )}
+      formQuestions={form4_1Questions(
+        formData,
+        handleChange,
+        handleSelectChange
+      )}
       handleSubmit={handleSubmit}
     />
   );
