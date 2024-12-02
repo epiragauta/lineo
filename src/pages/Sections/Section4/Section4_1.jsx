5b34a603f3b7b28c25b02c17229c9ccffa513a81
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-12-01 23:54:03 -0500

    Added local storage to form 4_1

--- src/pages/Sections/Section4/Section4_1.jsx
+++ src/pages/Sections/Section4/Section4_1.jsx
@@ -1,62 +1,61 @@
 // ./src/pages/Sections/Section4/Formulario4_1.js
 
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { questions4_1 as form4_1Questions } from "../../../questions/questions4/questions4_1"; // Importación del arreglo de componentes
 import { operationQuestions } from "../../../questions/operation_questions"; // Importación del arreglo de componentes
 import FormWrapper from "../../../components/FormWrapper"; // Asegúrate de tener este componente
 
+import useFormPersistence from "../../../hooks/useFormPersistence";
+import { AuthContext } from "../../../context/AuthContext";
+
 const Formulario4_1 = () => {
+  const { user } = useContext(AuthContext); // Get user from AuthContext
+  const userId = user ? user.id : null;
+
   const localStorageKey = "formulario4_1";
 
-  const [formData, setFormData] = useState(() => {
-    // Load data from LocalStorage
-    const savedData = localStorage.getItem(localStorageKey);
-    return savedData
-      ? JSON.parse(savedData)
-      : {
-          nombreOperacion: "",
-          dependencia: "",
-          registroAdmin: "",
-          objetivo: "",
-          poblacion: "",
-          anioInicio: "",
-          areaTematica: "",
-          periodicidad: "",
-          unidadEstudio: "",
-          variablesPrincipales: "",
-          coberturaGeografica: "",
-          periodoReferencia: "",
-          metodoRecoleccion: "",
-          respRecursosPregunta1: "",
-          respRecursosPregunta2: [],
-          respRecursosEvidencia1: "",
-          respRecursosEvidencia2: "",
-          respRecursosEvidencia3: "",
-          respMejoraPregunta1: "",
-          evaluacionPregunta1: 3,
-          respMejoraEvidencia1: "",
-          respMejoraEvidencia2: "",
-          respMejoraEvidencia3: "",
-          asignacionPregunta1: "",
-          asignacionPregunta2: [],
-          asignacionEvidencia1: "",
-          asignacionEvidencia2: "",
-          asignacionEvidencia3: "",
-          evaluacionPregunta2: 3,
-          evaluacionEvidencia1: "",
-          evaluacionEvidencia2: "",
-          evaluacionEvidencia3: "",
-          sugerenciasComentario: "",
-          accion: "",
-          responsableSNS: "",
-          fechaCumplimiento: "",
-        };
-  });
-
-  useEffect(() => {
-    // Save data to LocalStorage on every change
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  }, [formData]);
+  const [formData, setFormData] = useFormPersistence(
+    localStorageKey,
+    {
+      nombreOperacion: "",
+      dependencia: "",
+      registroAdmin: "",
+      objetivo: "",
+      poblacion: "",
+      anioInicio: "",
+      areaTematica: "",
+      periodicidad: "",
+      unidadEstudio: "",
+      variablesPrincipales: "",
+      coberturaGeografica: "",
+      periodoReferencia: "",
+      metodoRecoleccion: "",
+      respRecursosPregunta1: "",
+      respRecursosPregunta2: [],
+      respRecursosEvidencia1: "",
+      respRecursosEvidencia2: "",
+      respRecursosEvidencia3: "",
+      respMejoraPregunta1: "",
+      evaluacionPregunta1: 3,
+      respMejoraEvidencia1: "",
+      respMejoraEvidencia2: "",
+      respMejoraEvidencia3: "",
+      asignacionPregunta1: "",
+      asignacionPregunta2: [],
+      asignacionEvidencia1: "",
+      asignacionEvidencia2: "",
+      asignacionEvidencia3: "",
+      evaluacionPregunta2: 3,
+      evaluacionEvidencia1: "",
+      evaluacionEvidencia2: "",
+      evaluacionEvidencia3: "",
+      sugerenciasComentario: "",
+      accion: "",
+      responsableSNS: "",
+      fechaCumplimiento: "",
+    },
+    userId
+  );
 
   // Función para manejar cambios en inputs de tipo texto, radio y checkbox
   const handleChange = (e) => {
