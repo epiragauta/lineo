30d3bc05f261510bd944a99517d7a5bffce6c665
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-12-06 02:14:39 -0500

    Modified Dashboard Card, and added histogram and Pie chart examples

--- src/pages/sections/section4/section4_1/Dashboard4_1.jsx
+++ src/pages/sections/section4/section4_1/Dashboard4_1.jsx
@@ -1,16 +1,42 @@
 // src/pages/sections/section4/section4_1/Dashboard4_1.jsx
 
-import React, { useEffect, useState } from 'react';
-import { getNumberOfSubmissions } from '../../../../backend/api/numberOfSubmissions';
-import { FaClipboardList, FaChartBar } from 'react-icons/fa'; // Example icons
-import DashboardCard from '../../../../components/DashboardCard'; // Import DashboardCard
+import React, { useEffect, useState } from "react";
+import { getNumberOfSubmissions } from "../../../../backend/api/numberOfSubmissions";
+import { FaClipboardList, FaChartBar } from "react-icons/fa"; // Example icons
+import DashboardCard from "../../../../components/dashboard/DashboardCard";
+
+import PieChart from "../../../../components/dashboard/PieChart";
+import Histogram from "../../../../components/dashboard/Histogram";
 
 const Dashboard4_1 = ({ subsection, label }) => {
   const [submissionCount, setSubmissionCount] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const formId = subsection; 
+  const formId = subsection;
+
+  const histogramData = [
+    { category: "A", Si: 30, No: 20 },
+    { category: "B", Si: 50, No: 40 },
+    { category: "C", Si: 70, No: 60 },
+    { category: "D", Si: 90, No: 80 },
+  ];
+
+  const keys = ["Si", "No"];
+  const indexBy = "category";
+
+  const pieData = [
+    {
+      id: "Si",
+      label: "Si",
+      value: 70,
+    },
+    {
+      id: "No",
+      label: "No",
+      value: 30,
+    },
+  ];
 
   useEffect(() => {
     const fetchSubmissionCount = async () => {
@@ -48,8 +74,19 @@ const Dashboard4_1 = ({ subsection, label }) => {
         <DashboardCard
           icon={<FaChartBar className="text-green-600 w-6 h-6" />}
           title="Nivel"
-          value={2} // Static value as per your example
+          value={2} // TODO: Define formula to calculate level
         />
+      </div>
+
+      <div className="p-6 bg-gray-100">
+        <h2 className="text-xl font-semibold mb-4">Respuesta Sí/No</h2>
+        <PieChart data={pieData} />
+      </div>
+      <div className="p-6 bg-gray-100">
+        <h2 className="text-xl font-semibold mb-4">
+          Distribución por Categoría
+        </h2>
+        <Histogram data={histogramData} keys={keys} indexBy={indexBy} />
       </div>
     </div>
   );
