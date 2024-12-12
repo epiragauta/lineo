a19713a4a31f5800113bb5b0e26377b9181daf21
Daniel Perez <daniel.perez@pandas.com.co>
2024-12-12 02:02:26 -0500

    comment histogram

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -95,7 +95,7 @@ const Dashboard = ({ subsection, label, formQuestions }) => {
           { id: "Sí", label: "Sí", value: newRadioFrequencies["Sí"] ? newRadioFrequencies["Sí"] : 0 },
           { id: "No", label: "No", value: newRadioFrequencies["No"] ? newRadioFrequencies["No"] : 0 },
         ]);
-        setSliderFrequencies(Object.entries(newSliderFrequencies).map(([Puntaje, count]) => ({ Puntaje, Frecuencia: count })));
+        setSliderFrequencies( Object.entries(newSliderFrequencies).map(([Puntaje, count]) => ({ Puntaje, Frecuencia: count })) );
 
         // Compute section level using getScore utility
         const score = getScore(1, maxScore, sumScore);
@@ -148,12 +148,13 @@ const Dashboard = ({ subsection, label, formQuestions }) => {
       <PieChart key = "PieChart" data={radioFrequencies} label="Respuestas Sí/No" />
 
       {/* Histogram */}
-      <Histogram 
-        key = "HistogramChart" 
+      {/* <Histogram 
+        key = {"HistogramChart"} 
         data={sliderFrequencies} 
         keys={["Frecuencia"]} 
         indexBy="Puntaje" 
-        label={"Preguntas cuantitativas"}/>
+        label={"Preguntas cuantitativas"}
+        /> */}
       
       {/* grafica con datos de si y nos, es decir cantidad de si y nos sobre todas las preguntas usando newradio */}
       {/* grafica con de conteo de uno, dos tres usando los datos de newslider*/}
